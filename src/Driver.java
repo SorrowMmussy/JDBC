ee8760496951ea5d7f30077f1d69ccc1732a848a
SorrowMmUssY <denas.kinderis@gmail.com>
2018-05-09 16:54:26 +0300

    Connecting to SQL

--- src/Driver.java
+++ src/Driver.java
@@ -9,10 +9,15 @@ public class Driver {
 			// 1. Get a connection to database 
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
 			// 2. Create a statement
-			
+			Statement myStmt = myConn.createStatement();
 			// 3. Execute SQL query
+			ResultSet myRs = myStmt.executeQuery("select * from employees");
 			
 			// 4. Process the result set
+			
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + " " + myRs.getString("first_name"));
+			}
 		}
 		catch(Exception exc){
 			exc.printStackTrace();
